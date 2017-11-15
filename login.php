5dfa6dcf460df3c7838368b51cd6eed4e2d70ad4
Emad Bitar <bitar@blanko.de>
2017-11-15 17:09:22 +0100

    feat: add contact page and other

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+#element{
+    -webkit-animation: 3s ease 0s normal forwards 1 fadein;
+    animation: 3s ease 0s normal forwards 1 fadein;
+}
+
+@keyframes fadein{
+    0% { opacity:0; }
+    66% { opacity:0; }
+    100% { opacity:1; }
+}
+
+@-webkit-keyframes fadein{
+    0% { opacity:0; }
+    66% { opacity:0; }
+    100% { opacity:1; }
+}
+    </style>
+</head>
+
+<body>
+
+<div  id=element >  id=elementIf you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</date_interval_create_from_date_string>
+
+</body>
+</html>
